3588cf64bd25faf3b6058d20200fd139e25952b7
Your Name <ledesmalautaroezequiel02@gmail.com>
2025-01-15 15:53:11 -0300

    Added sectionheader component

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import CheckIcon from "@/assets/icons/check-circle.svg";
 import ArrowUpRighticon from "@/assets/icons/arrow-up-right.svg";
 import GrainImage from "@/assets/images/grain.jpg";
+import { SectionHeader } from "@/components/SectionHeader";
 
 const portfolioProjects = [
   {
@@ -49,17 +50,11 @@ export const ProjectsSection = () => {
   return (
     <section className="pb-16 lg:py-24">
       <div className="container">
-        <div className="flex justify-center">
-          <p className="uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400 text-transparent bg-clip-text text-center">
-            Real world results
-          </p>
-        </div>
-        <h2 className="font-serif text-3xl text-center mt-6 md:text-5xl">
-          Featured projects
-        </h2>
-        <p className="text-center text-white/60 mt-4 md:text-lg max-w-md mx-auto">
-          See how i transformed concepts into engaging digital experiences
-        </p>
+        <SectionHeader
+          eyebrow="Real world results"
+          title="Featured projects"
+          description="See how i transformed concepts into engaging digital experiences"
+        />
         <div className="flex flex-col gap-20 mt-10 md:mt-20">
           {portfolioProjects.map((project) => (
             <div
