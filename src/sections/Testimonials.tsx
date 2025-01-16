1bd3118ce1b19b6f0b530f3ac9c9e5801df4f7a6
Your Name <ledesmalautaroezequiel02@gmail.com>
2025-01-16 15:41:26 -0300

    Finished testimonial section

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -43,23 +43,36 @@ const testimonials = [
 
 export const TestimonialsSection = () => {
   return (
-    <div className="py-16">
+    <div className="py-16 lg:py-24">
       <div className="container">
         <SectionHeader
           eyebrow="Client testimonials"
           title="What clients say about me"
           description="Don't just take my word for it. Here's what my clients have to say."
         />
-        <div>
-          {testimonials.map((testimonial, index) => (
-            <Card key={index}>
-              
-              <Image src={testimonial.avatar} alt={testimonial.name} />
-              <div>{testimonial.name}</div>
-              <div>{testimonial.position}</div>
-              <p>{testimonial.text}</p>
-            </Card>
-          ))}
+        <div className="mt-16 lg:mt-24 flex overflow-x-clip [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
+          <div className="flex flex-none gap-8">
+            {testimonials.map((testimonial, index) => (
+              <Card key={index} className="max-w-xs md:p-8 md:max-w-md">
+                <div className="flex items-center gap-4">
+                  <div className="size-14 bg-gray-700 inline-flex items-center justify-items-center rounded-full flex-shrink-0">
+                    <Image
+                      src={testimonial.avatar}
+                      alt={testimonial.name}
+                      className="max-h-full"
+                    />
+                  </div>
+                  <div>
+                    <div className="font-semibold">{testimonial.name}</div>
+                    <div className="text-sm text-white/40">
+                      {testimonial.position}
+                    </div>
+                  </div>
+                </div>
+                <p className="mt-4 text-sm md:text-base">{testimonial.text}</p>
+              </Card>
+            ))}
+          </div>
         </div>
       </div>
     </div>
